f99b84bf1a1253ebf4a48e3c60bdab7468c0ed50
Lewis Reid <lreidwilliams@gmail.com>
2020-10-02 23:16:42 +1000

    refactored piece to break up large moves_from method

--- core/lib/checkers/piece.rb
+++ core/lib/checkers/piece.rb
@@ -6,20 +6,16 @@ module Checkers
     end
 
    def moves_from(position, game:)
+      jumps_exist = false
       Enumerator.new do |moves|
-        if @light
-          moves << position.bottom_left unless position.left_edge? || game.space_occupied?(position.bottom_left)
-          moves << position.bottom_right unless position.right_edge? || game.space_occupied?(position.bottom_right)
-        else
-          moves << position.top_left unless position.left_edge? || game.space_occupied?(position.top_left)
-          moves << position.top_right unless position.right_edge? || game.space_occupied?(position.top_right) do |piece|
-            potential_jump = position.top_right.top_right
-            moves << potential_jump unless game.space_occupied?(potential_jump)
-          end
+        jump_moves(position, game: game) do |jump|
+          moves << jump
+          jumps_exist = true
         end
+        regular_moves(position, game: game) { |move| moves << move } unless jumps_exist
       end
     end
-    
+
     def has_id?(id)
       @id == id
     end
@@ -46,6 +42,36 @@ module Checkers
 
     def own?(turn)
       turn.even? == @light
+    end
+
+    private
+
+    # TODO: Add tests for jumping when 1 away from the edge, likely the current code will break
+    # TODO: Feels like the logic for a jump and a regular move belongs together united under the direction being moved.
+    #       Refactoring is still needed!
+    def jump_moves(position, game:)
+      game.space_occupied?(position.send(right_direction)) do |piece|
+        potential_jump = position.send(right_direction).send(right_direction)
+        yield potential_jump unless game.space_occupied?(potential_jump)
+      end unless position.right_edge?
+      
+      game.space_occupied?(position.send(left_direction)) do |piece|
+        potential_jump = position.send(left_direction).send(left_direction)
+        yield potential_jump unless game.space_occupied?(potential_jump)
+      end unless position.left_edge?
+    end
+
+    def regular_moves(position, game:)
+      yield position.send(right_direction) unless position.right_edge? || game.space_occupied?(position.send(right_direction))
+      yield position.send(left_direction) unless position.left_edge? || game.space_occupied?(position.send(left_direction))
+    end
+
+    def right_direction
+      light? ? :bottom_right : :top_right
+    end
+
+    def left_direction
+      light? ? :bottom_left : :top_left
     end
   end
 end
--- core/spec/checkers/movement/single_jump_move_spec.rb
+++ core/spec/checkers/movement/single_jump_move_spec.rb
@@ -8,15 +8,19 @@ RSpec.describe Checkers::Game do
 
     context 'given a dark piece,' do
       let(:piece) { Checkers::Piece.new('dark', light: false) }
-      let(:position) { Checkers::Movement::Position.new(0, 0) }
       subject { game.moves_for(piece) }
 
       context 'with a jumpable light piece diagonally adjacent,' do
-        let(:other_pieces) {
-          { Checkers::Piece.new('light', light: true) => position.top_right }
-        }
+        let(:other_pieces) { { Checkers::Piece.new('light', light: true) => position.top_right } }
+        let(:position) { Checkers::Movement::Position.new(2, 0) }
         
         it { is_expected.to contain_exactly(position.top_right.top_right) }
+
+        context 'and the piece is next to the edge,' do
+          let(:position) { Checkers::Movement::Position.new(0, 0) }
+
+          it { is_expected.to contain_exactly(position.top_right.top_right) }
+        end
       end
     end
   end
