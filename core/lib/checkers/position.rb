c41c1df59f78a3e81be9321897c68ae96690831c
Lewis Reid <lreidwilliams@gmail.com>
2020-10-16 23:09:25 +1000

    simplified position internals using an attr reader for x and y coordinates

--- core/lib/checkers/position.rb
+++ core/lib/checkers/position.rb
@@ -4,8 +4,12 @@ module Checkers
   class Position
     LIMIT = 8
 
+    attr_reader :x, :y
+
     def initialize(x, y)
-      raise OutOfBounds.new(x: x, y: y) unless in_bounds?(x, y) && dark_square?(x, y)
+      in_bounds = x >= 0 && y >= 0 && x < LIMIT && y < LIMIT
+      dark_square = x.even? == y.even?
+      raise OutOfBounds.new(x: x, y: y) unless in_bounds && dark_square
 
       @x = x
       @y = y
@@ -16,23 +20,15 @@ module Checkers
     end
 
     def to_s
-      "(#{@x}, #{@y})"
-    end
-
-    def to_i
-      @x + (@y * LIMIT)
+      "(#{x}, #{y})"
     end
 
     def hash
-      self.to_i
-    end
-
-    def is_at?(x, y)
-      @x == x && y == @y
+      x + (y * LIMIT)
     end
 
     def ==(other)
-      other.is_a?(Position) && other.is_at?(@x, @y)
+      other.is_a?(Position) && other.x == x && other.y == y
     end
 
     def eql?(other)
@@ -40,81 +36,63 @@ module Checkers
     end
 
     def top_left
-      Position.new(@x.pred, @y.succ)
+      Position.new(x.pred, y.succ)
     end
 
     def top_right
-      Position.new(@x.succ, @y.succ)
+      Position.new(x.succ, y.succ)
     end
 
     def bottom_left
-      Position.new(@x.pred, @y.pred)
+      Position.new(x.pred, y.pred)
     end
 
     def bottom_right
-      Position.new(@x.succ, @y.pred)
+      Position.new(x.succ, y.pred)
     end
 
     def top_edge?
-      @y == LIMIT - 1
+      y == LIMIT - 1
     end
 
     def bottom_edge?
-      @y.zero?
+      y.zero?
     end
       
     def left_edge?
-      @x.zero?
+      x.zero?
     end
 
     def right_edge?
-      @x == LIMIT - 1
+      x == LIMIT - 1
     end
 
     def above?(position)
-      position.evaluate_y{ |y| y < @y }
+      y > position.y
     end
 
     def below?(position)
-      position.evaluate_y{ |y| y > @y }
+      y < position.y
     end
 
     def left_of?(position)
-      position.evaluate_x{ |x| x > @x }
+      x < position.x
     end
 
     def right_of?(position)
-      position.evaluate_x{ |x| x < @x }
+      x > position.x
     end
 
     def next_to?(position)
-      x_distance = position.evaluate_x{ |x| x - @x }.abs
-      y_distance = position.evaluate_y{ |y| y - @y }.abs
+      x_distance = (position.x - x).abs
+      y_distance = (position.y - y).abs
       x_distance + y_distance > 0 && x_distance <= 1 && y_distance <= 1
     end
 
     def move_towards(other)
-      x_dir = other.evaluate_x{|x| x - @x } <=> 0
-      y_dir = other.evaluate_y{|y| y - @y } <=> 0
-      Position.new(@x + x_dir, @y + y_dir)
-    end
-
-    def evaluate_x
-      yield(@x)
-    end
-
-    def evaluate_y
-      yield(@y)
-    end
-
-    private
-
-    def in_bounds?(x, y)
-      x >= 0 && y >= 0 && x < LIMIT && y < LIMIT
-    end
-
-    def dark_square?(x, y)
-      x.even? == y.even?
+      x_dir = other.x - x <=> 0
+      y_dir = other.y - y <=> 0
+      Position.new(x + x_dir, y + y_dir)
     end
   end
 end
