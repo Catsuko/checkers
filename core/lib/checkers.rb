9836ed565b9a83e70edd81ef77d900e64d602908
Lewis Reid <lreidwilliams@gmail.com>
2020-10-19 21:13:18 +1000

    removed domain models from entry point and added exception imports

--- core/lib/checkers.rb
+++ core/lib/checkers.rb
@@ -1,6 +1,3 @@
-# TODO: Do not require any domain objects here, instead require only ports that expose potential inputs
-require 'checkers/game'
-require 'checkers/player'
-require 'checkers/piece'
-require 'checkers/turn'
-require 'checkers/position'
+require 'checkers/out_of_bounds'
+require 'checkers/movement/out_of_turn'
+require 'checkers/movement/illegal_move'
