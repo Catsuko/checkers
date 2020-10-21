13f4097f891f9e2a6996a3b9b61bb1bde1183d19
Lewis Reid <lreidwilliams@gmail.com>
2020-10-21 22:28:14 +1000

    refactored call method of create new game service to trim some bulk

--- core/lib/checkers/services/create_new_game.rb
+++ core/lib/checkers/services/create_new_game.rb
@@ -15,18 +15,21 @@ module Checkers
         end
 
         @repository.generate_id.tap do |id|
-          @repository.store(
-            id: id,
-            first_player_id: first_player_id,
-            second_player_id: second_player_id,
-            **fresh_game(Checkers::Player.new(first_player_id), Checkers::Player.new(second_player_id)).to_h
-          )
-
+          @repository.store(fresh_game_attributes(id, first_player_id, second_player_id))
           @listener.handle_game_created(id)
         end
       end
 
       private
+
+      def fresh_game_attributes(game_id, first_player_id, second_player_id)
+        {
+          id: game_id,
+          first_player_id: first_player_id,
+          second_player_id: second_player_id,
+          **fresh_game(Checkers::Player.new(first_player_id), Checkers::Player.new(second_player_id)).to_h
+        }
+      end
 
       def fresh_game(first_player, second_player)
         Checkers::GameFactory.new.fresh(first_player, second_player)
