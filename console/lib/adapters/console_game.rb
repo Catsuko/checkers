d1b2c4f0b36037310703c71f138b6796a9306867
Lewis Reid <lreidwilliams@gmail.com>
2020-10-26 21:55:40 +1000

    refactoring input responsibilities into object

--- console/lib/adapters/console_game.rb
+++ console/lib/adapters/console_game.rb
@@ -15,15 +15,9 @@ module Adapters
       loop do
         game_state = @repository.get(@game_id)
         begin
-          id, move, modifier = *gets.chomp.split(' ')
-          piece_details = game_state.fetch(:pieces, {}).invert.detect{ |piece, pos| piece[:id].to_s == id }
-          next unless id && move && piece_details && movement_map.key?(move)
-
-          move_pos = piece_details.last + movement_map.fetch(move)
-          move_pos += movement_map.fetch(move) if modifier
-          move_command.(@game_id, id.to_i, move_pos)
+          move_command.(@game_id, *@input.select_move(game_state))
         rescue StandardError => e
-          puts e
+          puts e, "\n"
         end
       end
     end
@@ -42,15 +36,6 @@ module Adapters
     end
 
     private
-
-    def movement_map
-      @movement_map ||= {
-        'se' => 9,
-        'sw' => 7,
-        'ne' => -7,
-        'nw' => -9
-      }
-    end
 
     # TODO: Clean this hell up
     def render_game(game_state)
--- console/lib/input/console_input.rb
+++ console/lib/input/console_input.rb
@@ -3,5 +3,38 @@ module Input
     def player_names
       ['Green', 'White']
     end
+
+    def select_move(game_state)
+      piece_details = select_piece(game_state.fetch(:pieces))
+      [piece_details.first[:id], select_move_position(piece_details.last)]
+    end
+
+    private
+
+    def select_piece(pieces)
+      puts 'Which piece do you want to move?'
+      id = gets.chomp
+      pieces.invert.detect{ |piece, pos| piece[:id].to_s == id }.tap do |piece_details|
+        raise "'#{id}' does not match a piece, pick a number from the board" if piece_details.nil?
+      end
+    end
+
+    def select_move_position(from)
+      puts "Where do you want to move? #{movement_map.keys.join(' ')}"
+      from + movement_map.fetch(gets.chomp)
+    end
+
+    def movement_map
+      @movement_map ||= {
+        'se' => 9,
+        'sw' => 7,
+        'ne' => -7,
+        'nw' => -9,
+        'se jump' => 18,
+        'sw jump' => 14,
+        'ne jump' => -14,
+        'nw jump' => -18
+      }
+    end
   end
 end
